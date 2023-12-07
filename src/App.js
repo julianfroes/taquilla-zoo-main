f2c15a3d931d4c9f8d052122e91ead3010d0739f
Julian Rodriguez <112611772+julianfroes@users.noreply.github.com>
2023-12-06 20:57:04 -0700

    reintento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Logo from './img/zoo-logo.png';
 import LogoLetras from './img/logo-letras.png';
-import { Navbar, ToastContainer } from 'react-bootstrap';
+import { Navbar} from 'react-bootstrap';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Tickets from './components/Tickets';
-import Resume from './components/Resume';
-import SelectTickets from './components/clientpage/SelectTickets';
 import ClientInterface from './components/clientpage/ClientInterface';
 import Sales from './components/Sales/Sales';
 
